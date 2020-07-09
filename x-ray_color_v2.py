da62e91ec4b0ac747d36b3eb65be1b664d441a57
Kara Melih <karamel@astro.uni-bonn.de>
2020-07-09 17:32:55 +0200

    new version

--- x-ray_color_v2.py
+++ x-ray_color_v2.py
@@ -16,6 +16,8 @@ import matplotlib.image as mpimg
 import subprocess as sp
 import argparse
 import sys
+from scipy.ndimage import gaussian_filter
+from scipy.ndimage import median_filter
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -72,6 +74,8 @@ parser.add_argument('-ip', '--interpolation', nargs=1,
                     required=False,default=['bicubic'], help='interpolation, deafult: bicubic')
 parser.add_argument('-sm', '--smooth', nargs=1,
                     required=False,default=['None'], help='Gaussian smoothing eg: sm 3 , default is None')
+parser.add_argument('-sm_method', '--smooth_method', nargs=1,
+                    required=False, default=['G'], help='Smoothing method Gaussian(G) or Median(M) default is Gaussian \n ex: -sm_method G ')
 parser.add_argument('-cr', '--crop', nargs=1,
                     required=False,default=['False'], help='Crop the axis, labels, etc eg: cr True , default is False')
 parser.add_argument('-br', '--brightness', nargs=1,
@@ -82,6 +86,8 @@ parser.add_argument('-cc', '--cmap_check', nargs=1,
                     required=False,default=['N'], help='bring several cmap alternatives. syntax: -cc Y')
 parser.add_argument('-sc', '--smooth_check', nargs=1,
                     required=False,default=['N'], help='bring several smoothing alternatives. syntax: -sc Y')
+parser.add_argument('-vmin', '--vmin', nargs=1,
+                    required=False, default=[0.075], help='min value in logarithmic plot ex: -vmin 0.1')
 
 
 args = parser.parse_args()
@@ -94,23 +100,24 @@ conf = args.conf[0]
 cmap = args.cmap[0]
 ip = args.interpolation[0]
 sm = args.smooth[0]
+sm_method = args.smooth_method[0].lower()
 cr = args.crop[0]
 br = float(args.brightness[0])
 cmap_check = args.cmap_check[0]
 smooth_check = args.smooth_check[0]
+vmin = args.vmin[0]
+
+if sm_method != 'g' or sm_method != 'm':
+    print('Undefined smooth method! Only "G" or "M" allowed\n Using Gaussian')
+    sm_method = 'g'
 
 if '.' in outputname:
     outputname = outputname.split('.')[0]
 
 # set the coordinate system
 hdu = FITS.open(fits)
-wcs = WCS(hdu[0].header)
-
-try:
-    sp.call(["stiff", f"{fits}","-OUTFILE_NAME", f"{outputname}.tif","-c",f"{conf}"]);
-except:
-    print('Configuration file not found. Using default.')
-    sp.call(["stiff", f"{fits}","-OUTFILE_NAME", f"{outputname}.tif"]);
+wcs = WCS(hdu[0].header)         # world-coordinate-system
+img = hdu[0].data                # image data
 
 # define some initial-check functions
 def cmap_alternatives(imarray,br=br,ip=ip, binn=1):
@@ -123,95 +130,68 @@ def cmap_alternatives(imarray,br=br,ip=ip, binn=1):
     
     figure, [[a11,a12,a13],[a21,a22,a23],[a31,a32,a33]] = plt.subplots(nrows=3, ncols=3,figsize=(15,15))
     for cm,ax in zip(cmap_alternatives,[a11,a12,a13,a21,a22,a23,a31,a32,a33]):
-        cmap = plt.get_cmap(cm)
-        colored_img = cmap(imarray)
-        ax.imshow(colored_img[::binn,::binn]*br, interpolation=ip)
+        ax.imshow(np.flipud(imarray[::binn,::binn])*br, interpolation=ip, cmap=cm, norm=LogNorm(vmin=vmin, vmax=np.max(imarray)))
         ax.set_title(cm)
         ax.axis('off')
     figure.tight_layout() 
-    plt.savefig(outputname+'_alternative_cmaps.png')
+    plt.savefig(outputname+'+alternative_cmaps.png')
     
-def smooth_alternatives(imarray,br=br,ip=ip, binn=1, cm=cmap):
+def smooth_alternatives(imarray,br=br,ip=ip, binn=1, cm='cubehelix'):
     '''
     creates a 3x3 plots for different smoothing alternatives
-    '''
-    from scipy.ndimage import gaussian_filter
-    from scipy.ndimage import median_filter
-    
+    '''    
     imarray = imarray[::binn,::binn]
-    
-    cmap = plt.get_cmap(cm)
-    colored_img = cmap(imarray)
     
     median_filtered = []
     for window_size in [1,3,5]:
-        ims = []
-        for d in range(3):
-            im_conv_d = median_filter(colored_img[:,:,d], size=(d**2+window_size,d**2+window_size), mode='reflect')
-            ims.append(im_conv_d)
+        im = median_filter(imarray, size=window_size)
+        median_filtered.append(im)
 
-        im_conv = np.stack(ims, axis=2) #.astype("uint8")
-        median_filtered.append(im_conv)
-
-    smoothed = []
+    gaussian_filtered = []
     for sm in [1.5,3,5]:
         im = gaussian_filter(imarray, sigma=sm)
-        im = cmap(im)
-        smoothed.append(im)
+        gaussian_filtered.append(im)
         
-    alternatives = median_filtered + smoothed
+    alternatives = median_filtered + gaussian_filtered
     titles = ['median_filter:1','median_filter:3','median_filter:5','gaussian_filter:1.5','gaussian_filter:3','gaussian_filter:5',]
     figure, [[a11,a12,a13],[a21,a22,a23]] = plt.subplots(nrows=2, ncols=3,figsize=(15,10))
     for i,(sm,ax) in enumerate(zip(alternatives,[a11,a12,a13,a21,a22,a23])):
-        ax.imshow(sm, interpolation=ip)
+        ax.imshow(np.flipud(sm), cmap=cm, interpolation=ip, norm=LogNorm(vmin=vmin, vmax=np.max(imarray)))
         ax.set_title(titles[i])
         ax.axis('off')
     figure.tight_layout() 
-    plt.savefig(outputname+'_alternative_smoothing.png')
+    plt.savefig(outputname+'+alternative_cmaps.png')
 
-# read image into numpy-array ima
-imarray = mpimg.imread(outputname+'.tif')
 
 if (cmap_check != 'N') or (smooth_check != 'N'):
     if cmap_check != 'N':
         if sm != 'None':
-            from scipy.ndimage import gaussian_filter
-            imarray = gaussian_filter(imarray, sigma=float(sm))
-        cmap_alternatives(imarray, binn=10) # binning so its faster
+            if sm_method == 'g'
+                imarray = gaussian_filter(img, sigma=float(sm))
+            else:
+                imarray = median_filter(img, size=float(sm))
+        cmap_alternatives(img, binn=10) # binning so its faster
     if smooth_check != 'N':
-        smooth_alternatives(imarray,binn=10)
+        smooth_alternatives(img,binn=10)
     sys.exit()
 
 if sm != 'None':
-    from scipy.ndimage import gaussian_filter
-    imarray = gaussian_filter(imarray, sigma=float(sm))
+    if sm_method == 'g'
+        img = gaussian_filter(img, sigma=float(sm))
+    else:
+        img = median_filter(img, size=float(sm))
 
-# color the image (i.e. add rgba axis)
-cmap = plt.get_cmap(cmap)
-# print(imarray.shape)
-colored_img = cmap(imarray)
-# print(colored_img.shape)
-    
+
 #  draw figure
 fig = plt.figure(figsize=(10,10))
 ax = plt.subplot(111, projection=wcs)
-ax.imshow(np.flipud(colored_img*br), interpolation=ip)
+# show the image
+ax.imshow(img*br, interpolation=ip, cmap=cmap, norm=LogNorm(vmin=vmin, vmax=np.max(imarray)))
 
 if cr == 'True':
-    plt.tick_params(
-    axis='y',          # changes apply to the x-axis
-    which='both',      # both major and minor ticks are affected
-    bottom=False,      # ticks along the bottom edge are off
-    top=False,         # ticks along the top edge are off
-    labelbottom=False) # labels along the bottom edge are off
-
-    plt.tick_params(
-    axis='x',          # changes apply to the x-axis
-    which='both',      # both major and minor ticks are affected
-    bottom=False,      # ticks along the bottom edge are off
-    top=False,         # ticks along the top edge are off
-    labelbottom=False)
-    plt.tight_layout()
+    ax.tick_params(axis='x', which='both', bottom=False,  top=False, labelbottom=False)
+    ax.tick_params(axis='y', which='both', bottom=False,  top=False, labelbottom=False) 
+    plt.tight_layout() 
 else:
     raax = ax.coords[0]
     decax = ax.coords[1]
